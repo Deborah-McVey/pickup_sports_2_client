14f80331b9d4a67d5c73628d2c997fdd2b0d7963
Deborah McVey <deborah.mcvey@gmail.com>
2024-03-26 13:11:30 -0400

    more errors event-details

--- src/app/features/event-details/event-details.component.ts
+++ src/app/features/event-details/event-details.component.ts
@@ -1,31 +1,89 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventService } from '../../core/services/event.service';
+import { Event } from '../../shared/models/event';
 import { DatePipe } from '@angular/common';
-//import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../../core/services/user.service';
+import { User } from '../../shared/models/user';
 
 @Component({
   selector: 'app-event-details',
   standalone: true,
   imports: [DatePipe],
   templateUrl: './event-details.component.html',
-  styleUrl: './event-details.component.scss'
+  styleUrl: './event-details.component.scss',
 })
 export class EventDetailsComponent implements OnInit {
-  event:Event = new Event({});
+  event: Event = new Event({});
+  currentUser: User | null = new User({});
+  hasJoined: boolean = false;
+  guests: any = [];
 
-  constructor(private routes:ActivatedRoute, private eventService:EventService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private eventService: EventService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
-      this.routes.params.subscribe((params) =>{
+    this.route.params.subscribe((params) => {
+      console.log(params['id']);
       this.eventService.getEvent(params['id']).subscribe({
-          next: (event: Event) => {
+        next: (event: Event) => {
           this.event = event;
+          this.hasJoined = event.has_joined;
+          console.log('get event', this.event);
+          this.prepareGuests();
         },
         error: (error) => {
           console.log(error);
         },
-        });
       });
+    });
+
+    this.userService.currentUserSubject.subscribe(() => {
+      this.currentUser = this.userService.currentUserSubject.value;
+    });
+  }
+
+  toggleJoinEvent() {
+    // setup obs of join or leave
+    const eventJoin$ = this.hasJoined
+      ? this.eventService.leaveEvent(this.event.id)
+      : this.eventService.joinEvent(this.event.id);
+
+    eventJoin$.subscribe({
+      next: () => {
+        this.hasJoined = !this.hasJoined;
+
+        // prepare the guests
+        if(this.currentUser){
+          if(this.hasJoined){
+            this.event.participants.push(this.currentUser)
+          }else {
+            this.event.participants = this.event.participants.filter((p)=> p.id !== this.currentUser?.id)
+          }
+          this.prepareGuests();
+        }
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  prepareGuests() {
+    // references the guests via property
+    this.guests = [...this.event.participants];
+
+    const emptySlots = this.event.guests - this.event.participants.length;
+
+    for (let i = 0; i < emptySlots; i++) {
+      this.guests.push({ empty: true });
+    }
+  }
+
+  trackById(index: number, item:any){
+    return item.id || index;
   }
 }
