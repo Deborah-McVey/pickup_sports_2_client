05f827aded8bbc7e68113c0f18d9d4d3f9c25115
Deborah McVey <deborah.mcvey@gmail.com>
2024-03-25 20:34:14 -0400

    errors in event-details

--- src/app/features/event-details/event-details.component.ts
+++ src/app/features/event-details/event-details.component.ts
@@ -12,28 +12,15 @@ import { DatePipe } from '@angular/common';
   styleUrl: './event-details.component.scss'
 })
 export class EventDetailsComponent implements OnInit {
-  event: Event = new Event({});
-
-//export class EventDetailsComponent implements OnInit {
- // eventForm: Event = new FormGroup({
- // id: new FormControl('', [Validators.required]),
-  //  title: new FormControl('', [Validators.required]),
-   // content: new FormControl('', [Validators.required]),
-   // start_date_time: new FormControl('', [Validators.required]),
-   // end_date_time: new FormControl('', [Validators.required]),created_at: new FormControl('', [Validators.required]),
-   // sports: new FormControl('', [Validators.required]),
-   // user: new FormControl('', [Validators.required]),
-  //});
+  event:Event = new Event({});
 
   constructor(private routes:ActivatedRoute, private eventService:EventService) {}
 
   ngOnInit(): void {
       this.routes.params.subscribe((params) =>{
-        // console.log(params);
-        this.eventService.getEvent(params['id']).subscribe({
+      this.eventService.getEvent(params['id']).subscribe({
           next: (event: Event) => {
           this.event = event;
-          console.log('get event', this.event)
         },
         error: (error) => {
           console.log(error);
